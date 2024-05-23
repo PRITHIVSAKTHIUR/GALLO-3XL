e70723d195ab099f9cdd7d68b2ab2eddebfe845a
PRITHIV SAKTHI U R <117709109+PRITHIVSAKTHIUR@users.noreply.github.com>
2024-05-23 23:40:26 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+---
+title: DALL-E
+emoji: 🍪🌌
+colorFrom: purple
+colorTo: red
+sdk: gradio
+sdk_version: 4.31.4
+app_file: app.py
+pinned: true
+license: creativeml-openrail-m
+short_description: Dall-e 3
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ app.py
@@ -0,0 +1,316 @@
+#!/usr/bin/env python
+
+import os
+import random
+import uuid
+
+import gradio as gr
+import numpy as np
+from PIL import Image
+import spaces
+from typing import Tuple
+import torch
+from diffusers import StableDiffusionXLPipeline, EulerAncestralDiscreteScheduler
+
+DESCRIPTION = """# DALL-E 2K"""
+
+def save_image(img):
+    unique_name = str(uuid.uuid4()) + ".png"
+    img.save(unique_name)
+    return unique_name
+
+def randomize_seed_fn(seed: int, randomize_seed: bool) -> int:
+    if randomize_seed:
+        seed = random.randint(0, MAX_SEED)
+    return seed
+
+MAX_SEED = np.iinfo(np.int32).max
+
+if not torch.cuda.is_available():
+    DESCRIPTION += "\n<p>Running on CPU 🥶 This demo may not work on CPU.</p>"
+
+MAX_SEED = np.iinfo(np.int32).max
+
+USE_TORCH_COMPILE = 0
+ENABLE_CPU_OFFLOAD = 0
+
+
+if torch.cuda.is_available():
+    pipe = StableDiffusionXLPipeline.from_pretrained(
+        "prithivMLmods/NGC598-StableDiffusion-XL-v2",
+        torch_dtype=torch.float16,
+        use_safetensors=True,
+    )
+    pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)  
+    pipe.load_lora_weights("prithivMLmods/Cartwheel-DallE-3-Lora-Xl-v2", weight_name="dalle-3-xl-lora-v2.safetensors", adapter_name="dalle")
+    pipe.set_adapters("dalle")
+    pipe.to("cuda")
+
+
+    
+style_list = [
+    {
+        "name": "(No style)",
+        "prompt": "{prompt}",
+        "negative_prompt": "",
+    },
+
+    {
+    "name": "8K",
+    "prompt": "hyper-realistic 8K image of {prompt} . ultra-detailed, lifelike, high-resolution, sharp, vibrant colors, photorealistic",
+    "negative_prompt": "cartoonish, low resolution, blurry, simplistic, abstract, deformed, ugly",
+    },
+
+    {
+    "name": "4K",
+    "prompt": "hyper-realistic 4K image of {prompt} . ultra-detailed, lifelike, high-resolution, sharp, vibrant colors, photorealistic",
+    "negative_prompt": "cartoonish, low resolution, blurry, simplistic, abstract, deformed, ugly",
+    },
+
+    {
+    "name": "HDR Photography",
+    "prompt": "HDR photo of {prompt} . high dynamic range, vivid colors, sharp contrast, realistic, detailed, high resolution, professional",
+    "negative_prompt": "dull, low contrast, blurry, unrealistic, cartoonish, ugly, deformed",
+    },
+
+
+    {
+        "name": "Cinematic",
+        "prompt": "cinematic still {prompt} . emotional, harmonious, vignette, highly detailed, high budget, bokeh, cinemascope, moody, epic, gorgeous, film grain, grainy",
+        "negative_prompt": "anime, cartoon, graphic, text, painting, crayon, graphite, abstract, glitch, deformed, mutated, ugly, disfigured",
+    },
+    {
+        "name": "Photographic",
+        "prompt": "cinematic photo {prompt} . 35mm photograph, film, bokeh, professional, 4k, highly detailed",
+        "negative_prompt": "drawing, painting, crayon, sketch, graphite, impressionist, noisy, blurry, soft, deformed, ugly",
+    },
+    {
+        "name": "Anime",
+        "prompt": "anime artwork {prompt} . anime style, key visual, vibrant, studio anime,  highly detailed",
+        "negative_prompt": "photo, deformed, black and white, realism, disfigured, low contrast",
+    },
+    {
+        "name": "Manga",
+        "prompt": "manga style {prompt} . vibrant, high-energy, detailed, iconic, Japanese comic style",
+        "negative_prompt": "ugly, deformed, noisy, blurry, low contrast, realism, photorealistic, Western comic style",
+    },
+    {
+        "name": "Digital Art",
+        "prompt": "concept art {prompt} . digital artwork, illustrative, painterly, matte painting, highly detailed",
+        "negative_prompt": "photo, photorealistic, realism, ugly",
+    },
+    {
+        "name": "Pixel art",
+        "prompt": "pixel-art {prompt} . low-res, blocky, pixel art style, 8-bit graphics",
+        "negative_prompt": "sloppy, messy, blurry, noisy, highly detailed, ultra textured, photo, realistic",
+    },
+    {
+        "name": "Fantasy art",
+        "prompt": "ethereal fantasy concept art of  {prompt} . magnificent, celestial, ethereal, painterly, epic, majestic, magical, fantasy art, cover art, dreamy",
+        "negative_prompt": "photographic, realistic, realism, 35mm film, dslr, cropped, frame, text, deformed, glitch, noise, noisy, off-center, deformed, cross-eyed, closed eyes, bad anatomy, ugly, disfigured, sloppy, duplicate, mutated, black and white",
+    },
+    {
+        "name": "Neonpunk",
+        "prompt": "neonpunk style {prompt} . cyberpunk, vaporwave, neon, vibes, vibrant, stunningly beautiful, crisp, detailed, sleek, ultramodern, magenta highlights, dark purple shadows, high contrast, cinematic, ultra detailed, intricate, professional",
+        "negative_prompt": "painting, drawing, illustration, glitch, deformed, mutated, cross-eyed, ugly, disfigured",
+    },
+    {
+        "name": "3D Model",
+        "prompt": "professional 3d model {prompt} . octane render, highly detailed, volumetric, dramatic lighting",
+        "negative_prompt": "ugly, deformed, noisy, low poly, blurry, painting",
+    },
+
+    
+]   
+styles = {k["name"]: (k["prompt"], k["negative_prompt"]) for k in style_list}
+STYLE_NAMES = list(styles.keys())
+DEFAULT_STYLE_NAME = "(No style)"
+
+def apply_style(style_name: str, positive: str, negative: str = "") -> Tuple[str, str]:
+    p, n = styles.get(style_name, styles[DEFAULT_STYLE_NAME])
+    if not negative:
+        negative = ""
+    return p.replace("{prompt}", positive), n + negative
+
+@spaces.GPU(enable_queue=True)
+def generate(
+    prompt: str,
+    negative_prompt: str = "",
+    style: str = DEFAULT_STYLE_NAME,
+    use_negative_prompt: bool = False,
+    num_inference_steps: int = 30,
+    num_images_per_prompt: int = 2,
+    seed: int = 0,
+    width: int = 1024,
+    height: int = 1024,
+    guidance_scale: float = 3,
+    randomize_seed: bool = False,
+    progress=gr.Progress(track_tqdm=True),
+):
+
+    
+    seed = int(randomize_seed_fn(seed, randomize_seed))
+
+    if not use_negative_prompt:
+        negative_prompt = ""  # type: ignore
+    prompt, negative_prompt = apply_style(style, prompt, negative_prompt)
+
+    images = pipe(
+        prompt=prompt,
+        negative_prompt=negative_prompt,
+        width=width,
+        height=height,
+        guidance_scale=guidance_scale,
+        num_inference_steps=num_inference_steps,
+        num_images_per_prompt=num_images_per_prompt,
+        cross_attention_kwargs={"scale": 0.65},
+        output_type="pil",
+    ).images
+    image_paths = [save_image(img) for img in images]
+    print(image_paths)
+    return image_paths, seed
+
+examples = [
+    "a time traveler meeting their past self in a Victorian-era street",
+    "a carnival at night with colorful lights and whimsical rides",
+    "a Viking ship sailing through a storm with lightning in the background",
+    "a cyberpunk street market with neon lights and holographic signs",
+    "a space station orbiting a distant planet, serving as a hub for intergalactic travelers",
+    "a surreal landscape with floating islands and waterfalls cascading into the void"
+]
+
+css = '''
+.gradio-container{max-width: 560px !important}
+h1{text-align:center}
+footer {
+    visibility: hidden
+}
+'''
+with gr.Blocks(css=css, theme="xiaobaiyuan/theme_brief") as demo:
+    gr.Markdown(DESCRIPTION)
+    gr.DuplicateButton(
+        value="Duplicate Space for private use",
+        elem_id="duplicate-button",
+        visible=False,
+    )
+
+    with gr.Group():
+        with gr.Row():
+            prompt = gr.Text(
+                label="Prompt",
+                show_label=False,
+                max_lines=1,
+                placeholder="Enter your prompt",
+                container=False,
+            )
+            run_button = gr.Button("Run")
+        result = gr.Gallery(label="Result", columns=1, preview=True)
+    with gr.Accordion("Advanced options", open=False):
+        use_negative_prompt = gr.Checkbox(label="Use negative prompt", value=False, visible=True)
+        negative_prompt = gr.Text(
+                label="Negative prompt",
+                max_lines=1,
+                placeholder="Enter a negative prompt",
+                visible=True,
+            )
+        with gr.Row():
+            num_inference_steps = gr.Slider(
+                label="Steps",
+                minimum=10,
+                maximum=60,
+                step=1,
+                value=30,
+            )
+        with gr.Row():
+            num_images_per_prompt = gr.Slider(
+                label="Images",
+                minimum=1,
+                maximum=5,
+                step=1,
+                value=2,
+            )
+        seed = gr.Slider(
+            label="Seed",
+            minimum=0,
+            maximum=MAX_SEED,
+            step=1,
+            value=0,
+            visible=True
+        )
+        randomize_seed = gr.Checkbox(label="Randomize seed", value=True)
+        with gr.Row(visible=True):
+            width = gr.Slider(
+                label="Width",
+                minimum=512,
+                maximum=2048,
+                step=8,
+                value=1024,
+            )
+            height = gr.Slider(
+                label="Height",
+                minimum=512,
+                maximum=2048,
+                step=8,
+                value=1024,
+            )
+        with gr.Row():
+            guidance_scale = gr.Slider(
+                label="Guidance Scale",
+                minimum=0.1,
+                maximum=20.0,
+                step=0.1,
+                value=6,
+            )
+    with gr.Row(visible=True):
+            style_selection = gr.Radio(
+                show_label=True,
+                container=True,
+                interactive=True,
+                choices=STYLE_NAMES,
+                value=DEFAULT_STYLE_NAME,
+                label="Image Style",
+            )
+        
+
+    gr.Examples(
+        examples=examples,
+        inputs=prompt,
+        outputs=[result, seed],
+        fn=generate,
+        cache_examples=False,
+    )
+
+    use_negative_prompt.change(
+        fn=lambda x: gr.update(visible=x),
+        inputs=use_negative_prompt,
+        outputs=negative_prompt,
+        api_name=False,
+    )
+    
+
+    gr.on(
+        triggers=[
+            prompt.submit,
+            negative_prompt.submit,
+            run_button.click,
+        ],
+        fn=generate,
+        inputs=[
+            prompt,
+            negative_prompt,
+            style_selection,
+            use_negative_prompt,
+            num_inference_steps,
+            num_images_per_prompt,
+            seed,
+            width,
+            height,
+            guidance_scale,
+            randomize_seed,
+        ],
+        outputs=[result, seed],
+        api_name="run",
+    )
+    
+if __name__ == "__main__":
+    demo.queue(max_size=20).launch(show_api=False, debug=False)
